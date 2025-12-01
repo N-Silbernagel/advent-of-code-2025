429e91baacd676151239799479095bb4f8cf8fec
N-Silbernagel <6422477+N-Silbernagel@users.noreply.github.com>
2025-12-01 21:56:09 +0100

    Day01 part1

--- src/day01.zig
+++ src/day01.zig
@@ -8,39 +8,54 @@ const BitSet = std.DynamicBitSet;
 const util = @import("util.zig");
 const gpa = util.gpa;
 
-const data = @embedFile("data/day01.txt");
+const example =
+\\L68
+\\L30
+\\R48
+\\L5
+\\R60
+\\L55
+\\L1
+\\L99
+\\R14
+\\L82
+;
 
-pub fn main() !void {
-    
+const real = @embedFile("data/day01.txt");
+
+fn part1(input: []const u8) u16 {
+    var zeros: u16 = 0;
+    var position: u8 = 50;
+
+    var lines = std.mem.tokenizeAny(u8, input, "\r\n");
+
+    while (lines.next()) |line| {
+        const direction = line[0];
+        const number_str = line[1..];
+        const number = std.fmt.parseInt(u16, number_str, 10) catch std.debug.panic("Could not parse int {s}", .{number_str});
+
+        switch (direction) {
+            'L' => position = @intCast(@mod((@as(i16, position) - @as(i32, number)), 100)),
+            'R' => position = @intCast((position + number) % 100),
+            else => std.debug.panic("Unexpected direction: '{c}' (value: {d})", .{ direction, direction }),
+        }
+
+        if (position == 0) {
+            zeros += 1;
+        }
+    }
+
+    return zeros;
 }
 
-// Useful stdlib functions
-const tokenizeAny = std.mem.tokenizeAny;
-const tokenizeSeq = std.mem.tokenizeSequence;
-const tokenizeSca = std.mem.tokenizeScalar;
-const splitAny = std.mem.splitAny;
-const splitSeq = std.mem.splitSequence;
-const splitSca = std.mem.splitScalar;
-const indexOf = std.mem.indexOfScalar;
-const indexOfAny = std.mem.indexOfAny;
-const indexOfStr = std.mem.indexOfPosLinear;
-const lastIndexOf = std.mem.lastIndexOfScalar;
-const lastIndexOfAny = std.mem.lastIndexOfAny;
-const lastIndexOfStr = std.mem.lastIndexOfLinear;
-const trim = std.mem.trim;
-const sliceMin = std.mem.min;
-const sliceMax = std.mem.max;
+pub fn main() !void {
+    std.debug.print("{}", .{part1(real)});
+}
 
-const parseInt = std.fmt.parseInt;
-const parseFloat = std.fmt.parseFloat;
+test "correct part 1 example output" {
+    try std.testing.expectEqual(3, part1(example));
+}
 
-const print = std.debug.print;
-const assert = std.debug.assert;
-
-const sort = std.sort.block;
-const asc = std.sort.asc;
-const desc = std.sort.desc;
-
-// Generated from template/template.zig.
-// Run `zig build generate` to update.
-// Only unmodified days will be updated.
+test "correct part 1 real output" {
+    try std.testing.expectEqual(1135, part1(real));
+}
